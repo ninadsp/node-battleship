110da5743adcdd4faf3e5c413ff35faae8600508
ninadsp <ninadsp16289@gmail.com>
2012-09-02 02:36:35 +0530

    Configured socket.io, fixed typos

--- sockets.js
+++ sockets.js
@@ -3,11 +3,24 @@
  */
 
 var init = function(app, httpServer) {
+	console.log('sockets init');
 	io = require('socket.io').listen(httpServer);
+
+	io.configure(function() {
+			io.enable('browser client minification');
+			io.set('log level', 1);
+			io.set('transports', [
+					'websocket',
+					'htmlfile',
+					'xhr-polling',
+					'jsonp-polling'
+				]);
+			});
 
 	var Server = require(app.get('rootDirectory') + '/controllers/server.js');
 
 	var players = io.of('/battleships').on('connection', function(client) {
+			console.log('connection event for client : ' + client.id);
 			client.emit('clientId', {id: client.id});
 
 			client.on('bsCreateRoom', function(d) {
