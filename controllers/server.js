3f26486637a7bbb09af5c0917120066c29d24117
tvsam <tvsamartha@ymail.com>
2012-09-02 01:37:21 +0530

    JSLint code-refactoring

--- controllers/server.js
+++ controllers/server.js
@@ -1,102 +1,70 @@
 var Room = require("./Room");
 
-roomCounter = 0;
+var roomCounter = 0;
 var roomList = []; // Array containing all the roomIDs
 
-var createRoom = function()
-{
+var createRoom = function () {
 	var roomId = "r2d2" + (roomCounter++);
 	var room = Room(roomId);
 	roomList.push(room);
 	return roomId;
-}
+};
 
-var joinRoom = function(roomId, clientId)
-{
-	var room = null;
-	//var ret ;
-	for(var i = 0; i<roomList.length; i++)
-	{
-		if(roomList[i].getId() == roomID)
-		{
+var joinRoom = function (roomId, clientId) {
+	var room = null, i = 0;
+	for (i = 0; i < roomList.length; i++) {
+		if (roomList[i].getId() == roomId) {
 			room = roomList[i];
-			try
-			{
+			try {
 				room.addClient(clientId);
-			}
-			catch(e)
-			{
+			} catch (e) {
 				throw e;
 			}
 			break;
 		}
 	}
-
-	if(room == null)
-	{
+	if (room == null) {
 		throw "Sorry, I can't find this room on my Marauder's map !!! ";
 	}
-}
+};
 
-var placeShips = function(roomID, clientId, ships)
-{
-	var room = null;
-	//var ret ;
-	for(var i = 0; i<roomList.length; i++)
-	{
-		if(roomList[i].getId() == roomID)
-		{
+var placeShips = function (roomId, clientId, ships) {
+	var room = null, i = 0;
+	for (i = 0; i < roomList.length; i++) {
+		if (roomList[i].getId() == roomId) {
 			room = roomList[i];
-			try
-			{
+			try {
 				room.placeShips(clientId, ships);
-			}
-			catch(e)
-			{
+			} catch (e) {
 				throw e;
 			}
 			break;
 		}
 	}
-
-	if(room == null)
-	{
+	if (room == null) {
 		throw "Sorry, I can't find this room on my Marauder's map !!! ";
 	}
-	
-}
+};
 
-var fireShot = function(roomID, clientId, location)
-{
-	var room = null;
-	var ret ;
-	for(var i = 0; i<roomList.length; i++)
-	{
-		if(roomList[i].getId == roomID)
-		{
+var fireShot = function (roomId, clientId, location) {
+	var room = null, i = 0, ret;
+	for (i = 0; i < roomList.length; i++) {
+		if (roomList[i].getId == roomId) {
 			room = roomList[i];
-			try
-			{
+			try {
 				ret = room.fireShot(clientId, location); // returns JSON object {type: , location: , ship: }
 				return ret;
-			}
-			catch(e)
-			{
+			} catch (e) {
 				throw e;
 			}
 			break;
 		}
 	}
-		
-	if(room == null)
-	{
-		throw "Sorry, I can't find this room on my Marauder's map !!! ";
-	}
-}
+};
 
 exports.createRoom = createRoom;
 exports.joinRoom = joinRoom;
-exports.addClient = addClient;
+exports.placeShips = placeShips;
 exports.fireShot = fireShot;
 
 
